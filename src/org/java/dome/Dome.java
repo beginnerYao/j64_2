4d685d1c8bccfb722ae259b0011e30d735fe3a89
yc <1346559677@qq.com>
2020-06-29 17:14:01 +0800

    第三次提交------测试开发分支

--- src/org/java/dome/Dome.java
+++ src/org/java/dome/Dome.java
@@ -3,7 +3,7 @@ package org.java.dome;
 public class Dome {
 
   public static void main(String[] args) {
-    System.out.println("测试git");
+      System.out.println("测试git");
   }
 
 }
--- src/org/java/dome/Dome2.java
+++ src/org/java/dome/Dome2.java
@@ -4,6 +4,7 @@ public class Dome2 {
 
   public static void main(String[] args) {
     System.out.println("测试2----------开发分支");
+    System.out.println("可以传过去");
   }
 
 }
